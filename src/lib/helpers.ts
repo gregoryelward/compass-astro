b78f4907e567165ae674b7bd02b6bff102053c1d
prospercanada <gelward@prospercanada.org>
2024-04-17 08:23:30 -0400

    generate URL for benefit home page

--- src/lib/helpers.ts
+++ src/lib/helpers.ts
@@ -1,14 +1,26 @@
 export default function makeUrl(locale: string, name: string, ...args: any[]) {
+  // console.log('INPUT ', locale, name, [...args])
+
+  //href={`${ makeUrl(locale,
+  //   b.fields?.slug as string,
+  //   base,
+  //   getJurisdiction(b.fields.authority, locale)?.slug)
+  // }`}
+  //INPUT  en-CA canada-pension-plan-disability-benefit [ 'foobar-base', undefined ]
   const slugs = args.filter((f) => f)
+
+  // console.log('slugs 1 ', slugs)
 
   if (locale !== 'en-CA') {
     slugs.unshift('fr')
   }
+  // console.log('slugs 2 ', slugs)
 
   if (slugs.length === 0) {
     return `${name}`
   }
 
+  // console.log('slugs 3 ', `/${slugs.join('/')}/${name}`)
   return `/${slugs.join('/')}/${name}`
   // return `${slugs.join('/')}/${name}`
 }
--- src/lib/jurisdictions.js
+++ src/lib/jurisdictions.js
@@ -2,6 +2,7 @@ import jurisdictions from './jurisdictions.json'
 // console.log('jurisdictions ', jurisdictions.jurisdictions)
 
 export default function getJurisdiction(title, locale = `en-CA`) {
+  // console.log('title, locale ', title, locale)
   //   console.log(
   //     'jurisdictions.jurisdictions[locale] ',
   //     jurisdictions.jurisdictions[locale]
@@ -9,6 +10,10 @@ export default function getJurisdiction(title, locale = `en-CA`) {
   //   return 'xx'
   // console.log(
   //   'x ',
+  //   jurisdictions.jurisdictions[locale].find((f) => f.name === title)
+  // )
+  // console.log(
+  //   'jurisdictions.jurisdictions[locale].find((f) => f.name === title)',
   //   jurisdictions.jurisdictions[locale].find((f) => f.name === title)
   // )
   return jurisdictions.jurisdictions[locale].find((f) => f.name === title)
