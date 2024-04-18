f8de73776eeacff28f86a249799fcc6c4d635486
prospercanada <gelward@prospercanada.org>
2024-04-18 19:58:31 -0400

    fix url

--- src/lib/helpers.ts
+++ src/lib/helpers.ts
@@ -1,11 +1,11 @@
 export default function makeUrl(
+  base: string,
   locale: string,
-  base: string = '',
-  name?: string,
+  // name?: string,
   ...args: any[]
 ) {
   // locale : 'fr-CA' | 'en-CA'
-  // base:  "/" | "/compass-astro/"
+  // base:  "/" | "/compass-astro"
   // name:  remove this?
   // args: [] | ["sluga" | "slugb" | ...]
   // /fr
@@ -22,25 +22,25 @@ export default function makeUrl(
   //INPUT  en-CA canada-pension-plan-disability-benefit [ 'foobar-base', undefined ]
   const slugs = args.filter((f) => f)
 
-  console.log('slugs 1 ', slugs)
+  // console.log('slugs 1 ', slugs)
 
   if (locale !== 'en-CA') {
-    slugs.unshift('fr')
+    slugs.unshift('fr') // add
   }
   // if (base && base !== '/') {
   //   slugs.unshift(base)
   // }
-  console.log('slugs 2 ', slugs, slugs.length)
+  // console.log('slugs 2 ', slugs, slugs.length)
 
   // if (slugs.length === 0) {
   //   return `${name}`
   // }
   let url = `${base}${slugs.join('/')}`
 
-  if (name) {
-    url = url + name
-  }
-  console.log('name ', url)
+  // if (name) {
+  //   url = url + name
+  // }
+  // console.log('name ', url)
   return url
 
   // console.log('slugs 3 ', `${base}${slugs.join('/')}/${name}`)
