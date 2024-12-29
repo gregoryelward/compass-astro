1041e43ac367661796f9063f45e8629402dbd0a6
gregoryelward <gregoryelward@gmail.com>
2024-12-29 13:10:11 -0500

    new domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
-import { defineConfig } from 'astro/config';
-import sitemap from "@astrojs/sitemap";
+import { defineConfig } from 'astro/config'
+import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
 export default defineConfig({
-  // site: 'https://realtorjian.ca',
-  site: 'https://prospercanada.github.io',
+  site: 'https://realtorjian.ca',
+  // site: 'https://prospercanada.github.io',
   //
   // base: '/compass-astro/',
   base: process.env.NODE_ENV === 'production' ? '/compass-astro/' : '/',
@@ -17,7 +17,7 @@ export default defineConfig({
     }
   },
   integrations: [sitemap()]
-});
+})
 // https://gregoryelward.github.io/trove-astro/
 
 // export default defineConfig({
